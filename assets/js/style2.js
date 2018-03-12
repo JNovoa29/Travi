30c1cf8dded26e58393853599501cc9e7885202c
Jordan Novoa <jordan.novoa15@gmail.com>
2018-03-11 20:59:31 -0700

    visual updates, removed dest profile

--- assets/js/style2.js
+++ assets/js/style2.js
@@ -1,12 +1,30 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+// var header = $(".dest-title");
+// var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
   }
+  x[slideIndex-1].style.display = "block";  
 }
