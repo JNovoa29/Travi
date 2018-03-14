f759329bfb0314e0c1877efb38dc1f57d8ca4f9b
Lance Lee <lancechoelee@gmail.com>
2018-03-13 19:15:56 -0700

    changed how we add items on index2.html from table to appending to column

--- assets/js/eventbrite.js
+++ assets/js/eventbrite.js
@@ -10,7 +10,8 @@ $.ajax({
     var eventName = response.events[i].name.text
     var eventDesctription = response.events[i].description.text
     var eventDatetime = response.events[i].start.local
-    var eventURL = response.events[i].url
+    var eventURL = response.events[i].vanity_url
+    var eventURLText = eventURL
 
     var addEvent = '<tr>' +
     '<td>' + '<a href="' + eventURL + '">' + eventName + '</a>' + '</td>' +
@@ -18,7 +19,17 @@ $.ajax({
     '<td>' + eventDatetime + '</td>' + 
     '</tr>'
 
-    $('.events').append(addEvent)
+    if (eventURL === undefined){
+      eventURL = response.events[i].url
+      eventURLText = response.events[i].url
+    }
+
+    $('.event-name').append('<p class="event-name-item">' + eventName + '</p>')
+    $('.event-link').append('<p><a href="' + eventURL + '">' + eventURLText + '</a></p>')
+    $('.event-time').append('<p class="event-time-item">' + eventDatetime + '</p>')
+
+
+
   }
   
 
